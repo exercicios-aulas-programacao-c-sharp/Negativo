23c74d933fa67d8b558ba204d6a0d90a5ca0c773
diego_neri <diego@diegoneri.com>
2020-06-17 18:07:40 -0300

    Finaliza exercício Negativo

--- Program.cs
+++ Program.cs
@@ -6,7 +6,13 @@ namespace Negativo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int numero;
+            Console.Write("Digite um número: ");
+            numero = Convert.ToInt32(Console.ReadLine());
+
+            if (numero < 0){
+                Console.WriteLine("Você digitou um número negativo.");
+            }
         }
     }
 }
